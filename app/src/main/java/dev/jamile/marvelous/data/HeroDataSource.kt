ab84ec8f2d368421990ee5305c935c0f17c0bbe7
Jamile <jamilelima@protonmail.com>
2020-04-08 22:07:53 -0300

    refactor: change files location

--- app/src/main/java/dev/jamile/marvelous/data/HeroDataSource.kt
+++
@@ -1,7 +0,0 @@
-package dev.jamile.marvelous.data
-
-import dev.jamile.marvelous.data.model.HeroResponse
-
-interface HeroDataSource {
-    fun listAllHeroes(success : (List<HeroResponse>) -> Unit, failure: () -> Unit)
-}
--- app/src/main/java/dev/jamile/marvelous/data/remote/HeroApi.kt
+++
@@ -1,2 +0,0 @@
-package dev.jamile.marvelous.data.remote
-
